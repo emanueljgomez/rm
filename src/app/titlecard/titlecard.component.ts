2296578340dea332e1782f545c9b9cfd130f5b01
Emanuel Gomez <emanueljgomez@gmail.com>
2021-04-28 17:00:14 -0300

    Created new component for Title Card

---
+++ src/app/titlecard/titlecard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titlecard',
+  templateUrl: './titlecard.component.html',
+  styleUrls: ['./titlecard.component.css']
+})
+export class TitlecardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/titlecard/titlecard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitlecardComponent } from './titlecard.component';
+
+
+
+@NgModule({
+  declarations: [
+    TitlecardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TitlecardModule { }
