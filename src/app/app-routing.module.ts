a70f1c31c325ea91ae25bc789e5929ce4e6041bf
Emanuel Gomez <emanueljgomez@gmail.com>
2021-04-30 13:10:02 -0300

    Added redirection for root path to 'Home' component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ReactiveComponent } from './reactive/reactive.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'reactive', component: ReactiveComponent }
 ];
